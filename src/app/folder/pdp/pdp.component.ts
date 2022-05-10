ddd46b12036b7e1ebc24ec28cd3d450798484f3c
GoblinSasquatch <60318434+goblinsasquatch@users.noreply.github.com>
2022-05-10 14:41:44 +0100

    Add pdp (#1)
    * pdp template added
    
    * pdp feature complete

---
+++ src/app/folder/pdp/pdp.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pdp',
+  templateUrl: './pdp.component.html',
+  styleUrls: ['./pdp.component.scss'],
+})
+export class PdpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
